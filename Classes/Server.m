bcdee4b908d732f3b385384226316bcf7e1b32e9
Serg Bayda <sbayda@gmail.com>
2009-11-22 13:40:08 +0200

    Fixes leaks in test datas and fixes button style in BrowserController

--- Classes/Server.m
+++ Classes/Server.m
@@ -51,15 +51,15 @@
 #ifdef STUB
 - (void)_onTimer:(id)unused
 {
-	Tag *c = [[Tag alloc] initWithTagname:@"Penda of Mercia" url:@"http://en.wikipedia.org/wiki/Penda_of_Mercia"];
-	Tag *b = [[Tag alloc] initWithTagname:@"Oswiu of Northumbria" url:@"http://en.wikipedia.org/wiki/Oswiu_of_Northumbria"];
-	Tag *d = [[Tag alloc] initWithTagname:@"Battle of Winwaed" url:@"http://en.wikipedia.org/wiki/Battle_of_Winwaed"];
-	NSArray *testarray = [[NSArray alloc] initWithObjects:d, c, b, nil];
+	Tag *c = [[[Tag alloc] initWithTagname:@"Penda of Mercia" url:@"http://en.wikipedia.org/wiki/Penda_of_Mercia"] autorelease];
+	Tag *b = [[[Tag alloc] initWithTagname:@"Oswiu of Northumbria" url:@"http://en.wikipedia.org/wiki/Oswiu_of_Northumbria"] autorelease];
+	Tag *d = [[[Tag alloc] initWithTagname:@"Battle of Winwaed" url:@"http://en.wikipedia.org/wiki/Battle_of_Winwaed"] autorelease];
+	NSArray *testarray = [[[NSArray alloc] initWithObjects:d, c, b, nil] autorelease];
 	Event *e = [[[Event alloc] initWithName:@"655 – Battle of Winwaed: Penda of Mercia is defeated by Oswiu of Northumbria" date:[NSDate date] tags:testarray] autorelease];
-	Tag *a = [[Tag alloc] initWithTagname:@"Battle of Morgarten" url:@"http://en.wikipedia.org/wiki/Battle_of_Morgarten"];
-	Tag *z = [[Tag alloc] initWithTagname:@"Schweizer Eidgenossenschaft" url:@"http://en.wikipedia.org/wiki/Swiss_Confederation"];
-	Tag *x = [[Tag alloc] initWithTagname:@"Leopold I" url:@"http://en.wikipedia.org/wiki/Leopold_I,_Duke_of_Austria"];
-	NSArray *testarray1 = [[NSArray alloc] initWithObjects:a, z, x, nil];
+	Tag *a = [[[Tag alloc] initWithTagname:@"Battle of Morgarten" url:@"http://en.wikipedia.org/wiki/Battle_of_Morgarten"] autorelease];
+	Tag *z = [[[Tag alloc] initWithTagname:@"Schweizer Eidgenossenschaft" url:@"http://en.wikipedia.org/wiki/Swiss_Confederation"] autorelease];
+	Tag *x = [[[Tag alloc] initWithTagname:@"Leopold I" url:@"http://en.wikipedia.org/wiki/Leopold_I,_Duke_of_Austria"] autorelease];
+	NSArray *testarray1 = [[[NSArray alloc] initWithObjects:a, z, x, nil] autorelease];
 	Event *w = [[[Event alloc] initWithName:@"1315 – Battle of Morgarten the Schweizer Eidgenossenschaft ambushes the army of Leopold I" date:[NSDate date] tags:testarray1] autorelease];
 	
 	[_events addObject:e];
