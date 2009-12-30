172ed968faba9850a4d425e94bb82b8b8f386852
Vladimir Pouzanov <farcaller@gmail.com>
2009-12-30 18:50:08 +0200

    Added parser test

--- Tests/ServerTest.m
+++ Tests/ServerTest.m
@@ -14,6 +14,7 @@
 
 - (void)_fetchURL:(NSURL *)url invoking:(DataBlock)block;
 - (NSData *)_fetchURL:(NSURL *)url;
+- (BOOL)_parseData:(NSData *)htmlData;
 
 @end
 
@@ -43,6 +44,31 @@
 	STAssertEqualObjects(html, [[NSUserDefaults standardUserDefaults] valueForKey:@"cacheData"], @"Data cached is not same as data stored");
 	fhtml = [s _fetchURL:url];
 	STAssertEqualObjects(html, fhtml, @"Data fetched from cache is not same as data stored");
+}
+
+- (void)testServerParse
+{
+	NSString *path = [[NSBundle mainBundle] pathForResource:@"December_13" ofType:@"html"];
+	NSData *html = [NSData dataWithContentsOfFile:path];
+	
+	Server *s = [Server sharedInstance];
+	STAssertNotNil(s, @"Server hasn't inited itself!");
+	BOOL parseOk = [s _parseData:html];
+	STAssertTrue(parseOk, @"Parser failed somehow");
+	STAssertTrue([s.events count] == 254, @"Supposed to parse 254 events, but got %d", [s.events count]);
+	STAssertTrue([s.list count] == 3, @"Supposed to parse 3 categories, but got %d", [s.list count]);
+	NSDictionary *routed = [NSDictionary dictionaryWithObjectsAndKeys:
+							[NSNumber numberWithInt:34], @"Events",
+							[NSNumber numberWithInt:145], @"Birth",
+							[NSNumber numberWithInt:75], @"Death",
+							nil];
+	for(NSDictionary *d in s.list) {
+		NSNumber *n = [routed objectForKey:[d objectForKey:@"Title"]];
+		STAssertNotNil(n, @"Unexpected category %@", [d objectForKey:@"Title"]);
+		NSArray *e = [d objectForKey:@"Objects"];
+		STAssertTrue([e count] == [n intValue], @"Expected %d objects in %@, but got %d",
+					 [n intValue], [d objectForKey:@"Title"], [e count]);
+	}
 }
 
 @end
